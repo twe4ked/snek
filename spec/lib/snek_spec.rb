c87c5403deae38302f23f7464c14b67fceee0875
Odin Dutton <odindutton@gmail.com>
2016-07-10 15:19:15 +0930

    Add Snek spec

---
+++ spec/lib/snek_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../lib/snek'
+
+RSpec.describe Snek do
+  describe '#<<' do
+    it 'does not grow longer than the snek_length' do
+      snek = Snek.new([[0, 0]])
+
+      (1..9).each do |i|
+        snek << [0, i]
+      end
+
+      expect(snek).to eq [[0, 5], [0, 6], [0, 7], [0, 8], [0, 9]]
+    end
+  end
+end

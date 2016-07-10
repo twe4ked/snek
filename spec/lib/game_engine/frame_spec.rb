37acef426438409320b019e505a20c6bce15a632
Odin Dutton <odindutton@gmail.com>
2016-07-10 16:23:02 +0930

    Improve frame specs

--- spec/lib/game_engine/frame_spec.rb
+++ spec/lib/game_engine/frame_spec.rb
@@ -2,36 +2,47 @@ require_relative '../../../lib/game_engine/frame'
 
 RSpec.describe GameEngine::Frame do
   describe '#draw' do
-    let(:frame) { GameEngine::Frame.new 5, 1 }
+    let(:frame) { GameEngine::Frame.new 5, 3 }
+    let(:blank_5_x_3_frame) do
+      [
+        '     ',
+        '     ',
+        '     ',
+      ]
+    end
 
     it 'draws in the range' do
-      frame.draw 1, 0, 'foo'
+      frame.draw 1, 1, 'foo'
 
-      expect(frame.rows).to eq [' foo ']
+      expect(frame.rows).to eq [
+        '     ',
+        ' foo ',
+        '     ',
+      ]
     end
 
     it 'draws off the right hand side' do
       frame.draw 10, 0, 'foo'
 
-      expect(frame.rows).to eq ['     ']
+      expect(frame.rows).to eq blank_5_x_3_frame
     end
 
     it 'draws at the right edge' do
       frame.draw 5, 0, 'f'
 
-      expect(frame.rows).to eq ['     ']
+      expect(frame.rows).to eq blank_5_x_3_frame
     end
 
     it 'draws off the bottom' do
-      frame.draw 0, 1, 'f'
+      frame.draw 0, 3, 'f'
 
-      expect(frame.rows).to eq ['     ']
+      expect(frame.rows).to eq blank_5_x_3_frame
     end
 
     it 'draws off the top' do
-      frame.draw 0, -2, 'f'
+      frame.draw 0, -1, 'f'
 
-      expect(frame.rows).to eq ['     ']
+      expect(frame.rows).to eq blank_5_x_3_frame
     end
   end
 end
